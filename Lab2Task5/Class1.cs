94ca3992501c5cabebd38c67fa363ee29e415ac7
carissia <reyecari@kean.edu>
2023-02-07 21:55:13 -0500

    Lab2

---
+++ Lab2Task5/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+    namespace Lab2Task5
+{  
+    public class Class1
+    {   /* Carissia Reyes
+         * Lab2
+         * Task5
+         * Cps3330*0 */
+        /// <summary>
+        /// this method calculates the max
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static void myMax(int num1, int num2)
+        {
+            if (num1 < num2)
+            {
+                Console.WriteLine("The maximun = " +num2);
+            }
+            else
+            {
+                Console.WriteLine("The maximun = " + num1);
+            }
+        }
+            public static void myMax1(int a, int b, int c)
+        {
+            int[] myNums = { a, b, c };
+            int maximum = myNums.Max();
+            Console.WriteLine("Maximum = {0} ", maximum);
+            
+        }
+    }
+}
+
+
---
+++ testl1t5/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Carissia Reyes
+ * Lab2Task7
+ * cps3330*03
+ * */
+namespace Lab2Task5
+{  
+
+    public class TestL2tt
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a value for a");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Class1.myMax(a, b);
+
+            Console.WriteLine("Enter a value for x");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for y");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for z");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            int[] k = { x, y, z };
+
+            Class1.myMax1(x, y, z);
+        }
+    }
+}
